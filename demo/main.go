216f2322275c5032d99feb84ce93fa6d2d5461ef
brunonsocha <brunon.socha@gmail.com>
2026-01-19 12:42:38 +0100

    still setting it up

--- demo/main.go
+++ demo/main.go
@@ -9,14 +9,29 @@ import (
 
 func main() {
 	containerId := os.Getenv("NODE_ID")
+	if containerId == "" {
+		var err error
+		containerId, err = os.Hostname()
+		if err != nil {
+			log.Fatalf("[Error] %v", err)
+		}
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[Container %s] Received a request...", containerId)
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "[Container %s] Hello!\n", containerId)
 	})
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	mux.HandleFunc("GET /id", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, containerId)
+	})
 	log.Printf("[Container %s] Starting the server on port :8080", containerId)
 	http.ListenAndServe(":8080", mux)
 
 }
+
 
